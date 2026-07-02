518a956688cb9425b1a31d9d7714bbdd803c51d1
folgue02 <folgue@proton.me>
2026-07-02 13:35:12 +0200

    FIX: Releases wouldn't contain the version in their filename

--- create_release.sh
+++ create_release.sh
@@ -4,12 +4,15 @@ set -e
 RELEASE_DIR=./release
 BUILDS_DIR=./builds
 
+# Last tag version
+LAST_TAG=$(git tag --list | head -n 1)
+
 # Clean build directory before compiling.
 if [ -f $BUILDS_DIR ]
 then
     rm -rf $BUILDS_DIR
 fi
-php baker app:build baker.php --build-version=$(git tag --list | head -n 1)
+php baker app:build baker.php --build-version=$LAST_TAG
 composer bin
 
 if [ ! -f $RELEASE_DIR ]
@@ -22,7 +25,7 @@ fi
 for file in $(find "$BUILDS_DIR/build" -type f -perm +111)
 do
     target_filename=$(basename $file)
-    target_path="$RELEASE_DIR/baker-$target_filename"
+    target_path="$RELEASE_DIR/baker-$LAST_TAG-$target_filename"
     echo "Copying '$file' into '$target_path'"
     cp "$file" "$target_path"
 done
